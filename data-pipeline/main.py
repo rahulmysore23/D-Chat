64efb5c96f3ffffede42e26402323994cc193cb2
adithya70 <adithyadhoni.70@gmail.com>
2024-09-29 03:36:11 -0500

    Updating the data scraping script

--- data-pipeline/main.py
+++ data-pipeline/main.py
@@ -3,39 +3,39 @@ from scraping import setup_driver, collect_hrefs, scrape_page
 from file_operations import save_content_locally
 from upload_to_pinata import upload_to_pinata
 import os
+import argparse
 
-def main():
-    # Load environment variables
+def main(search_term, limit):
     load_dotenv()
     pinata_jwt = os.getenv('PINATA_JWT')
-    #driver_path = #Chrome driver path
-    url = 'https://ask.usda.gov/s/global-search/food?tabset-fd9ce=2'
-    output_dir = "usdac_data_scrap"
+    url = f'https://ask.usda.gov/s/global-search/{search_term}?tabset-fd9ce=2'
+    output_dir = f"usdac_data_scrap_{search_term}"
+    driver_path = "D:\chromedriver-win64\chromedriver-win64\chromedriver.exe" # update this before running
 
-    # Set up WebDriver
     driver = setup_driver(driver_path)
 
     try:
-        # Collect HREFs
-        hrefs = collect_hrefs(driver, url, limit=10)
+        hrefs = collect_hrefs(driver, url, limit=limit)
         print(f"Collected {len(hrefs)} hrefs")
 
-        # Scrape content
         all_contents = []
         for count, href in enumerate(hrefs, start=1):
             page_content = scrape_page(driver, href)
             all_contents.append({"href": href, "content": page_content})
             print(f"Scraped link {count}: {href}")
 
-        # Save content locally
-        save_content_locally(all_contents, output_dir)
+        save_content_locally(all_contents, output_dir, search_term)
 
     finally:
-        # Quit driver
         driver.quit()
 
-    # Upload to Pinata
     upload_to_pinata(output_dir, pinata_jwt)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Scrape USDA website and upload to Pinata')
+    parser.add_argument('search_term', type=str, help='Search term for USDA website')
+    parser.add_argument('-l', '--limit', type=int, default=10, help='Limit of links to scrape (default: 10)')
+    
+    args = parser.parse_args()
+    
+    main(args.search_term, args.limit)
